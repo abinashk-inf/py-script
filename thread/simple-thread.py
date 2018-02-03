9752b9a3bd11ec7a17af95443808b1dfb9050b2e
abinashk-inf <abinashkr94@gmail.com>
2018-02-04 01:16:11 +0530

    start and join threads

--- thread/simple-thread.py
+++ thread/simple-thread.py
@@ -1,2 +1,17 @@
-import threading
+import threading as T
 
+def add(na,nb):
+    s=na+nb
+    print(s)
+def mult(na,nb):
+    m=na*nb
+    print(m)
+
+if __name__=='__main__':
+    t1=T.Thread(target=add,args=(10,20))
+    t2=T.Thread(target=mult,args=(10,20))
+    t1.start()
+    t2.start()
+    t1.join()
+    t2.join()
+    print('EOD')
