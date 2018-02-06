7cbe146b75bd999ccba602b60c990712098b47e4
abinashk-inf <abinashkr94@gmail.com>
2018-02-06 15:00:35 +0530

    Network details

--- wifi-details/wifi.py
+++ wifi-details/wifi.py
@@ -10,14 +10,30 @@ name = wi.Id
 typ=wi.Type
 print('Network: {}'.format(name))
 print('Network Type: {}'.format(typ))
+print("")
 
 ip4=bus.get('.NetworkManager',con.Ip4Config)
 ip4Add=ip4.AddressData
-print('IpV4 Address: {}'.format(ip4Add))
+ip4Routes=ip4.Routes
+ip4RtData=ip4.RouteData
+ip4Nameserver=ip4.Nameservers
+print('IpV4 Details:')
+print('     Address: {}'.format(ip4Add))
+print('     Routes: {}'.format(ip4Routes))
+print('     Route Data: {}'.format(ip4RtData))
+print('     Nameserver: {}'.format(ip4Nameserver))
 
 ip6=bus.get('.NetworkManager',con.Ip6Config)
 ip6Add=ip6.AddressData
-print('IpV6 Address: {}'.format(ip6Add))
+ip6Routes=ip6.Routes
+ip6RtData=ip6.RouteData
+ip6Nameserver=ip6.Nameservers
+print('IpV6 Details:')
+print('     Address: {}'.format(ip6Add))
+print('     Routes: {}'.format(ip6Routes))
+print('     Route Data: {}'.format(ip6RtData))
+print('     Nameserver: {}'.format(ip6Nameserver))
+print('')
 
 dev=bus.get('.NetworkManager',nm.GetDevices()[1])
 mac=dev.HwAddress
