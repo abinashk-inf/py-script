af00526587b597121239cc5f3f159c4b5ec58fec
abinashk-inf <abinashkr94@gmail.com>
2018-02-06 14:41:50 +0530

    Network Password

--- wifi-details/wifi.py
+++ wifi-details/wifi.py
@@ -2,24 +2,30 @@ from pydbus import SystemBus
 
 bus=SystemBus()
 nm=bus.get('.NetworkManager')
+active=nm.ActiveConnections
+con=bus.get('.NetworkManager',active[0])
 
 wi=bus.get('.NetworkManager',nm.ActiveConnections[0])
 name = wi.Id
 typ=wi.Type
-print(name)
-print(typ)
+print('Network: {}'.format(name))
+print('Network Type: {}'.format(typ))
 
 ip4=bus.get('.NetworkManager',con.Ip4Config)
 ip4Add=ip4.AddressData
-print(ip4Add)
+print('IpV4 Address: {}'.format(ip4Add))
 
 ip6=bus.get('.NetworkManager',con.Ip6Config)
 ip6Add=ip6.AddressData
-print(ip6Add)
+print('IpV6 Address: {}'.format(ip6Add))
 
 dev=bus.get('.NetworkManager',nm.GetDevices()[1])
 mac=dev.HwAddress
-print(mac)
+print('Physical Address: {}'.format(mac))
+
+pas=bus.get('.NetworkManager',wi.Connection)
+secret=pas.GetSecrets(pas.GetSettings()['802-11-wireless']['security'])['802-11-wireless-security']['psk']
+print('Network Password: {}'.format(secret))
 
 
 
