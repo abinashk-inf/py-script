4e2e08341be427f0a1415046eb636e1c113ef755
abinashk-inf <abinashkr94@gmail.com>
2018-02-06 14:25:08 +0530

    ipv4 and ipv6 address

--- wifi-details/wifi.py
+++ wifi-details/wifi.py
@@ -9,3 +9,12 @@ typ=wi.Type
 print(name)
 print(typ)
 
+ip4=bus.get('.NetworkManager',con.Ip4Config)
+ip4Add=ip4.AddressData
+print(ip4Add)
+
+ip6=bus.get('.NetworkManager',con.Ip6Config)
+ip6Add=ip6.AddressData
+print(ip6Add)
+
+
